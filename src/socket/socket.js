d3f5f0fa6e41d5583e6689d61571f933cce38c45
Farveen <farveenfoxiom@gmail.com>
2026-09-24 13:27:49 +0530

    Update API and socket URLs to use production endpoint

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL =
   import.meta.env.VITE_SOCKET_URL ||
-  "http://localhost:5000";
+  "https://chatapplication-backend-qrxy.onrender.com/api";
 
 const socket = io(SOCKET_URL, {
   autoConnect: false,
