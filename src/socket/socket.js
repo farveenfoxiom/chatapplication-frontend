a01782408eb5bc90ea3afb5d7fed6f3aa3cdf94f
Farveen <farveenfoxiom@gmail.com>
2026-09-25 09:27:07 +0530

    Fix SOCKET_URL to remove unnecessary API path for socket connection

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL =
   import.meta.env.VITE_SOCKET_URL ||
-  "https://chatapplication-backend-qrxy.onrender.com/api";
+  "https://chatapplication-backend-qrxy.onrender.com";
 
 const socket = io(SOCKET_URL, {
   autoConnect: false,
